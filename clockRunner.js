3c853da0291a963445a7e7c4b2bc7a49c2bcd1cb
Ian Ybarra <50596097+IanYbarra2000@users.noreply.github.com>
2020-07-22 08:36:20 -0500

    add time formatting, 12 and 24 hour display options, font, and background images

--- clockRunner.js
+++ clockRunner.js
@@ -1,6 +1,52 @@
+let time12=true;
 function updateTime(){
     let time= new Date();
-    document.getElementById('clock').innerHTML=time.getHours()+":"+time.getMinutes()+":"+ time.getSeconds();
+    let hour=time.getHours();
+    let minute=time.getMinutes();
+    let second=time.getSeconds();
+    let half='';
+    //format hours to conform to 12 hour time
+    if(time12){
+        if(time.getHours()===0) {
+            hour=12;
+        }
+        else if(time.getHours()<=12){
+            hour=time.getHours();
+        }
+        else {
+            hour=(time.getHours()-12);
+        }
+    }
+    //format minutes and seconds to always be two digits
+    if(time.getMinutes()<10){
+        minute="0"+time.getMinutes();
+    }
+    if(time.getSeconds()<10){
+        second="0"+time.getSeconds();
+    }
+
+    //sets which half of the day it is
+    if(time12){
+        if(time.getHours()<12){
+            half='AM';
+        }
+        else {
+            half='PM';
+        }
+    }
+    //set the html of clock to the current time
+    document.getElementById('clock').innerHTML=hour+":"+minute+":"+second+' '+half;
     setTimeout(updateTime,1000);
+}
+document.getElementById('12').addEventListener('change',changeTimeType);
+document.getElementById('24').addEventListener('change',changeTimeType);
+
+function changeTimeType() {
+    if(document.getElementById('12').checked){
+        time12=true;
+    }
+    else {
+        time12=false;
+    }
 }
 updateTime();
